e7a95a3bd468912f7b0d82c7955d80db8ad6627f
Loren Lisk <loren.lisk@foghornconsulting.com>
2020-07-07 21:33:17 -0400

    changed a bit more.

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,11 +6,9 @@ echo "Downloading \"s3://${S3_BUCKET}/${ENVIRONMENT}/\" to \"${LOCAL_DEST}\""
 /usr/bin/aws s3 sync "s3://${S3_BUCKET}/${ENVIRONMENT}/" "${LOCAL_DEST}" ;
 
 chown -R 1001:1001 "${LOCAL_DEST}" ;
-chmod go=rx,u=rwx "${LOCAL_DEST}" ;
 
+find ${LOCAL_DEST} -type d -exec chmod 'go=rx,u=rwx' {} \;
 find ${LOCAL_DEST} -type f -exec chmod 'u=rwx,go=rx' {} \;
 find ${LOCAL_DEST} -type f -exec ls -alh {} \;
-
-
 
 echo "Files transfered"
