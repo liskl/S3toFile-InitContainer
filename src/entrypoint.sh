4085b87946f9a7d7751cb4377a092bb1a8ef13fc
Loren Lisk <loren.lisk@foghornconsulting.com>
2020-07-09 13:05:34 -0400

    added version.txt use.

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,8 +2,15 @@
 
 set -euo pipefail;
 
-echo "Downloading \"s3://${S3_BUCKET}/${ENVIRONMENT}/\" to \"${LOCAL_DEST}\""
-/usr/bin/aws s3 sync "s3://${S3_BUCKET}/${ENVIRONMENT}/" "${LOCAL_DEST}" ;
+echo "Downloading \"s3://${S3_BUCKET}/${ENVIRONMENT}/version.txt\" to \"${LOCAL_DEST}\""
+/usr/bin/aws s3 cp "s3://${S3_BUCKET}/${ENVIRONMENT}/version.txt" "${LOCAL_DEST}" ;
+
+TIMESTAMP="$(cat "${LOCAL_DEST}/version.txt")"
+
+echo "Downloading \"s3://${S3_BUCKET}/${ENVIRONMENT}/\*-${TIMESTAMP}.sql.gz" to \"${LOCAL_DEST}\""
+/usr/bin/aws s3 sync "s3://${S3_BUCKET}/${ENVIRONMENT}/" "${LOCAL_DEST}" --exclude "*" --include "*-${TIMESTAMP}.sql.gz";
+
+rm -rf "${LOCAL_DEST}/version.txt";
 
 chown -R 1001:1001 "${LOCAL_DEST}" ;
 
